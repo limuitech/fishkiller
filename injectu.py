bc8e3d1c1626601ae74b86053e86c0aad7b2d7ec
TonyChyi <tonychee1989@gmail.com>
2015-05-09 22:11:17 +0800

    python版增加检测功能

--- injectu.py
+++ injectu.py
@@ -3,6 +3,9 @@
 
 import urllib
 import urllib2
+import re
+
+REG = "location.href='(down.asp)'"
 
 ADDRESS = 'http://jfh.10086yux.com/submit.asp'
 
@@ -22,12 +25,18 @@ POST = {
 data = urllib.urlencode(POST)
 req = urllib2.Request(ADDRESS, data)
 n = 0
+r = re.compile(REG)
 while True:
     response = urllib2.urlopen(req)
     try:
         if response.getcode() == 200:
-            n += 1
-            print u'已轰炸%d次' % n
+            nk = r.findall(response.read())
+            if nk[0] == 'down.asp':
+                n += 1
+                print u'已轰炸%d次' % n
+            else:
+                print u'目测被墙'
+                break
     except urllib2.HTTPError as e:
         print e.code
     except KeyboardInterrupt:
