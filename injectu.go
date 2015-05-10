b1ed756aad05177356f75a6073a7ae6fa13bebd1
TonyChyi <tonychee1989@gmail.com>
2015-05-10 16:55:19 +0800

    fmt

--- injectu.go
+++ injectu.go
@@ -4,16 +4,16 @@ import (
 	"encoding/base64"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"net/url"
 	"os"
+	"regexp"
 	"runtime"
 	"strconv"
 	"strings"
 	"time"
-    "regexp"
-    "io/ioutil"
 )
 
 const ADD = "http://jfh.10086yux.com/submit.asp"
@@ -26,7 +26,7 @@ var duration int
 func init() {
 	flag.IntVar(&procs, "proc", 1, "Start n processes.")
 	flag.IntVar(&timeout, "timeout", 10, "Set timeout")
-    flag.IntVar(&duration, "freq", 1000, "Set access frequency (ms)")
+	flag.IntVar(&duration, "freq", 1000, "Set access frequency (ms)")
 	flag.Parse()
 }
 
@@ -37,7 +37,7 @@ func main() {
 	client.Timeout = time.Second * time.Duration(timeout)
 	for i := 0; i < procs; i++ {
 		go whoop(client, ch, i, duration)
-        time.Sleep(time.Duration(duration) * time.Millisecond)
+		time.Sleep(time.Duration(duration) * time.Millisecond)
 	}
 	counter := 0
 	for {
@@ -49,15 +49,15 @@ func main() {
 			continue
 		}
 	}
-    defer func() {
-        if x:=recover(); x!=nil{
-			    fmt.Fprintf(os.Stderr, "访问被阻止，程序退出")
-        }
-    }()
+	defer func() {
+		if x := recover(); x != nil {
+			fmt.Fprintf(os.Stderr, "访问被阻止，程序退出")
+		}
+	}()
 }
 
 func whoop(client *http.Client, ch chan int, proc int, duration int) {
-    rn := regexp.MustCompile(REG)
+	rn := regexp.MustCompile(REG)
 	for {
 		vals := make(url.Values)
 		vals.Set("idType", "1")
@@ -137,7 +137,7 @@ func whoop(client *http.Client, ch chan int, proc int, duration int) {
 		vals.Set(
 			"sja",
 			//fmt.Sprintf("%04d", strconv.Itoa(rand.Int()%10000)),
-            "2018",
+			"2018",
 		)
 
 		req, err := http.NewRequest(
@@ -157,20 +157,20 @@ func whoop(client *http.Client, ch chan int, proc int, duration int) {
 			continue
 		}
 		if d.StatusCode == 200 {
-            body, err := ioutil.ReadAll(d.Body)
-            if err != nil{
-			    fmt.Fprintf(os.Stderr, "Proc %d Got Err: %v\n", proc, err)
-            }
-            na := rn.FindAllString(string(body), -1)
-            if len(na) > 0{
-			    ch <- 1
-            } else {
-			    fmt.Fprintf(os.Stderr, "访问被阻止，程序退出")
-                os.Exit(1)
-            }
-        } else {
+			body, err := ioutil.ReadAll(d.Body)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Proc %d Got Err: %v\n", proc, err)
+			}
+			na := rn.FindAllString(string(body), -1)
+			if len(na) > 0 {
+				ch <- 1
+			} else {
+				fmt.Fprintf(os.Stderr, "访问被阻止，程序退出")
+				os.Exit(1)
+			}
+		} else {
 			fmt.Fprintf(os.Stderr, "Proc %d Got Err: %s\n", proc, d.Status)
 		}
-        time.Sleep(time.Duration(duration) * time.Millisecond)
+		time.Sleep(time.Duration(duration) * time.Millisecond)
 	}
 }
